492df219ffa72ca0ab2f10f9bc6c813edc87e1fd
paolobrogli <Portone88>
2020-05-09 14:29:43 +0200

    Bookstore FrontEnd

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,13 @@
+export class Book {
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    inutsInStock: number;
+    createdOn: Date;
+    updatedOn: Date;
+
+}
